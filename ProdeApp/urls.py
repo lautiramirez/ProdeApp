3cd0013d5efbccfcc73b2b530f208ee6dbcc8035
lautiramirez <lramirez@mi.unc.edu.ar>
2022-11-29 21:34:07 -0300

    main-page mockup

--- ProdeApp/urls.py
+++ ProdeApp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from prode.views import main_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page),
+
 ]
